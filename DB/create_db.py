a2d9b035a56ccac1e1deffc8bfc6e65bd8cbadf6
backsty <shalamovartem6@mail.ru>
2024-03-09 02:17:43 +0700

    create_db branch origin/main

---
+++ DB/create_db.py
@@ -0,0 +1,104 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = "Users"
+
+    id = sq.Column(sq.INTEGER, primary_key=True)
+    name = sq.Column(sq.VARCHAR(128), nullable=False)
+    surname = sq.Column(sq.VARCHAR(128))
+    sex = sq.Column(sq.VARCHAR(128))
+    age = sq.Column(sq.INTEGER)
+    city = sq.Column(sq.VARCHAR(256))
+    vk_profile_id = sq.Column(sq.INTEGER, sq.ForeignKey("Photos.id"), nullable=False)
+    photos = relationship("Photos", back_populates="user")
+    blocked_status = relationship("Blocklist", uselist=False, back_populates="user")
+    favourites = relationship("Favourites", back_populates="user")
+    likes = relationship("Likes", back_populates="user")
+
+    def __str__(self):
+        return f"Users {self.id}: {self.name} {self.surname} {self.vk_profile_id}"
+
+
+class Blocklist(Base):
+    __tablename__ = "Blocklist"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey("Users.id"))
+    user = relationship("Users", back_populates="blocked_status")
+    is_blocked = sq.Column(sq.Boolean)
+
+    def __str__(self):
+        return f"User {self.user_id} Status: Blocked - {self.is_blocked}"
+
+
+class Photos(Base):
+    __tablename__ = "Photos"
+
+    id = sq.Column(sq.INTEGER, primary_key=True)
+    user_id = sq.Column(sq.INTEGER, nullable=False)
+    user = relationship("Users", back_populates="photos")
+    photo_url = sq.Column(sq.String(256))
+    upload_date = sq.Column(sq.TIMESTAMP, server_default=sq.func.now())
+
+    def __str__(self):
+        return f"Users {self.id}: {self.user_id} {self.photo_url}"
+
+
+class Likes(Base):
+    __tablename__ = "Likes"
+
+    id = sq.Column(sq.INTEGER, primary_key=True)
+    photo_id = sq.Column(sq.INTEGER, sq.ForeignKey("Photos.id"), nullable=False)
+    status_like = sq.Column(sq.BOOLEAN)
+    user_id = sq.Column(sq.INTEGER, sq.ForeignKey("Users.id"))
+    user = relationship("Users", back_populates="likes")
+
+    def __str__(self):
+        return f"Users {self.user_id}: {self.photo_id} {self.id} {self.status_like}"
+
+
+class Favourites(Base):
+    __tablename__ = "Favourites"
+
+    id = sq.Column(sq.INTEGER, primary_key=True)
+    user_id = sq.Column(sq.INTEGER, sq.ForeignKey("Users.id"), nullable=False)
+    user = relationship("Users", back_populates="favourites")
+    photo_id = sq.Column(sq.VARCHAR(128), sq.ForeignKey("Photos.id"), nullable=False)
+
+    def __str__(self):
+        return f"Users {self.id}: {self.user_id} {self.photo_id}"
+
+
+class Matches(Base):
+    __tablename__ = "Matches"
+
+    id = sq.Column(sq.INTEGER, primary_key=True)
+    user_id_receiver = sq.Column(sq.INTEGER, sq.ForeignKey("Users.id"))
+    user_id_sender = sq.Column(sq.INTEGER, sq.ForeignKey("Users.id"))
+
+    def __str__(self):
+        return f"Users {self.id}: {self.user_id_receiver} - {self.user_id_sender}"
+
+
+class Search_Weights(Base):
+    __tablename__ = "Search_Weights"
+
+    id = sq.Column(sq.INTEGER, primary_key=True)
+    user_id = sq.Column(sq.INTEGER, sq.ForeignKey("Users.id"))
+    age_weight = sq.Column(sq.INTEGER)
+    gender_weight = sq.Column(sq.VARCHAR(128))
+    common_groups = sq.Column(sq.VARCHAR(128))
+    common_friends = sq.Column(sq.VARCHAR(128))
+    music_interest_weight = sq.Column(sq.VARCHAR(128))
+    book_interest_weight = sq.Column(sq.VARCHAR(128))
+
+    def __str__(self):
+        return f"Users {self.id}: {self.user_id}"
+
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
